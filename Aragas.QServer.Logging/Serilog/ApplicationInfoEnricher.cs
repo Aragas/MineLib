f2f03a8d824b92f7ae7543e84a1b83f835670e7f
Vitaly Mikhailov <personal@aragas.org>
2020-02-20 16:52:36 +0300

    Last changes

--- Aragas.QServer.Logging/Serilog/ApplicationInfoEnricher.cs
+++ Aragas.QServer.Logging/Serilog/ApplicationInfoEnricher.cs
@@ -11,6 +11,7 @@ namespace Aragas.QServer.Logging.Serilog
     {
         public const string ApplicationPropertyName = "Application";
         public const string ApplicationUidPropertyName = "ApplicationUid";
+        public const string EnvironmentPropertyName = "Environment";
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static LogEventProperty CreateApplicationProperty(ILogEventPropertyFactory propertyFactory) =>
@@ -20,10 +21,15 @@ namespace Aragas.QServer.Logging.Serilog
         private static LogEventProperty CreateApplicationUidProperty(ILogEventPropertyFactory propertyFactory, Guid applicationUid) =>
             propertyFactory.CreateProperty(ApplicationUidPropertyName, applicationUid);
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static LogEventProperty CreateEnvironmentProperty(ILogEventPropertyFactory propertyFactory) =>
+            propertyFactory.CreateProperty(EnvironmentPropertyName, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "UNKNOWN");
+
         private readonly Guid _applicationUid;
 
         private LogEventProperty? _cachedApplicationNameProperty;
         private LogEventProperty? _cachedApplicationUidProperty;
+        private LogEventProperty? _cachedEnvironmentProperty;
 
         public ApplicationInfoEnricher(Guid applicationUid)
         {
@@ -34,6 +40,7 @@ namespace Aragas.QServer.Logging.Serilog
         {
             logEvent.AddPropertyIfAbsent(GetApplicationLogEventProperty(propertyFactory));
             logEvent.AddPropertyIfAbsent(GetApplicationUidLogEventProperty(propertyFactory));
+            logEvent.AddPropertyIfAbsent(GetEnvironmentLogEventProperty(propertyFactory));
         }
 
         private LogEventProperty GetApplicationLogEventProperty(ILogEventPropertyFactory propertyFactory) =>
@@ -41,5 +48,8 @@ namespace Aragas.QServer.Logging.Serilog
 
         private LogEventProperty GetApplicationUidLogEventProperty(ILogEventPropertyFactory propertyFactory) =>
             _cachedApplicationUidProperty ??= CreateApplicationUidProperty(propertyFactory, _applicationUid);
+
+        private LogEventProperty GetEnvironmentLogEventProperty(ILogEventPropertyFactory propertyFactory) =>
+            _cachedEnvironmentProperty ??= CreateEnvironmentProperty(propertyFactory);
     }
 }
